934e633da5b1ba06cc6a99be46db60265df1242d
Kanny <kanny.ghafour87@gmail.com>
2023-07-16 18:47:44 -0500

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('form');
+  form.addEventListener('submit', e=>{
+    e.preventDefault()
+    toDo(e.target.new_task_description.value)
+    form.reset()
+  })
+
+}); 
+
+function toDo(todo){
+  const li = document.createElement('li')
+  li.textContent = `${todo}  `
+  const btn = document.createElement('button')
+  btn.addEventListener('click',deleteToDo)
+  btn.textContent = 'X'
+  li.appendChild(btn)
+  const ul = document.querySelector('ul')
+  ul.appendChild(li)
+ 
+}
+
+function deleteToDo(e){
+e.target.parentNode.remove()
+}
